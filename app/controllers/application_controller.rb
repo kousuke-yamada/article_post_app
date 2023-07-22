0ccfa1f5a4892cb1de083215c288a46f11595d0e
kousuke.yamada <offmailbox1225@gmail.com>
2023-07-22 22:54:14 +0900

    Add devise login system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_user_new_permitted_parameters, if: :devise_controller?
+  before_action :configure_user_update_permitted_parameters, if: :devise_controller?
+
+  private
+    # Sign up
+    def configure_user_new_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
+    # Edit User
+    def configure_user_update_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # deviseがメールを送る際のホスト名を指定
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # letter_opener_webのセットアップ
+  config.action_mailer.delivery_method = :letter_opener_web
 end
