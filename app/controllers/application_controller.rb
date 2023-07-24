0c48e3f359b5fab2d8c722189435ac33bc81bb3f
kousuke.yamada <offmailbox1225@gmail.com>
2023-07-24 20:39:06 +0900

    root change to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_user_new_permitted_parameters, if: :devise_controller?
   before_action :configure_user_update_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    users_dash_boards_path
+  end
+
   private
     # Sign up
     def configure_user_new_permitted_parameters
